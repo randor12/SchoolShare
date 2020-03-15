12371aac07e7b312a8c422027f04a74bf1f11035
randor12 <rynicholas@vt.edu>
2020-03-15 14:47:47 -0400

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+npm install -g node-modules
+npm install express
+npm install body-parser
+npm install mysql
+npm install mongoose
+touch auth.json
+printf "{\n \"user\": \"user here\",\n\"pass\": \"pass here\"\n}" >> auth.json
+chmod 777 setup.sh
