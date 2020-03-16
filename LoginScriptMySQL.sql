f314584d99decbadda339356be6c5c8b1746be7e
randor12 <rynicholas@vt.edu>
2020-03-15 21:37:20 -0400

    Table Script for SQL
    Added SQL Script to initialize Table

---
+++ LoginScriptMySQL.sql
@@ -0,0 +1,18 @@
+use schoolshare_schema;
+CREATE DATABASE IF NOT EXISTS `nodelogin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `nodelogin`;
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `salt` varchar(8) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com', 'testSalt');
+
+ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
+ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+
+SELECT * FROM accounts where email = "test@gmail.com" and password = "t3stP@ss0rd";
