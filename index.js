30152a6792b69db0d2dd722d34e681e81eeb451b
randor12 <rynicholas@vt.edu>
2020-03-21 17:54:31 -0400

    fixed salt format

--- index.js
+++ index.js
@@ -132,13 +132,16 @@ app.post('/auth', function (request, response) {
 
 // Get salt for password
 function saltPass(length) {
+    var salt = crypto.genSaltSync(10);
+    /*
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return result;
+    */
+    return salt;
 }
 
 function hashPasswd(passwd, salt) {
