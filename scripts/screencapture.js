4a6522b03ffed59f1fc0f3d220d314e3d21973b3
Matt Gonley <50349733+mattgonley@users.noreply.github.com>
2020-03-28 17:36:04 -0400

    screen capture possibility

---
+++ scripts/screencapture.js
@@ -0,0 +1,25 @@
+// JavaScript source code
+// https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture
+// https://github.com/webrtcHacks/adapter
+async function startCapture(gdmOptions) {
+  let captureStream = null;
+
+  try {
+    captureStream = await navigator.mediaDevices.getDisplayMedia(gdmOptions);
+  } catch(err) {
+    console.error("Error: " + err);
+  }
+  return captureStream;
+}
+
+//constants passed into get media display
+const gdmOptions = {
+  video: {
+    cursor: "never" // display cursor
+  },
+  audio: {
+    echoCancellation: true,
+    noiseSuppression: true,
+    sampleRate: 44100
+  }
+}
