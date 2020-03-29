43b33e7570ab621789409eb1c4b339cec17e0459
randor12 <rynicholas@vt.edu>
2020-03-29 17:32:39 -0400

    video

--- scripts/video.js
+++ scripts/video.js
@@ -5,6 +5,7 @@ function getRoomID() {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
+    // Creates 10 character random ROOM ID number 
     for (var i = 0; i < 10; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
@@ -78,4 +79,120 @@ function getVideo() {
         .catch(function (err) {
             console.log(err.name + ": " + err.message);
         });
+}
+
+
+function setUpConnection(channelID) {
+    var localConnection = new RTCPeerConnection();
+
+    var sendChannel = localConnection.createDataChannel(channelID);
+    sendChannel.onopen = handleSendChannelStatusChange;
+    sendChannel.onclose = handleSendChannelStatusChange;
+
+    var remoteConnection = new RTCPeerConnection();
+    remoteConnection.ondatachannel = receiveChannelCallback;
+
+    localConnection.onicecandidate = e => !e.candidate || 
+        remoteConnection.addIceCandidate(e.candidate).catch();
+
+    remoteConnection.onicecandidate = e => !e.candidate
+        || localConnection.addIceCandidate(e.candidate)
+            .catch(handleAddCandidateError);
+    
+    localConnection.createOffer()
+        .then(offer => localConnection.setLocalDescription(offer))
+        .then(() => remoteConnection.setRemoteDescription(localConnection.localDescription))
+        .then(() => remoteConnection.createAnswer())
+        .then(answer => remoteConnection.setLocalDescription(answer))
+        .then(() => localConnection.setRemoteDescription(remoteConnection.localDescription))
+        .catch(handleCreateDescriptionError);
+
+    
+}
+
+var connectButton = document.querySelector('.connect')
+var disconnectButton = document.querySelector('.disconnect');
+
+function handleLocalAddCandidateSuccess() {
+    connectButton.disabled = true;
+}
+
+function handleRemoteAddCandidateSuccess() {
+    disconnectButton.disabled = false;
+}
+
+function receiveChannelCallback(event) {
+    receiveChannel = event.channel;
+    receiveChannel.onmessage = handleReceiveMessage;
+    receiveChannel.onopen = handleReceiveChannelStatusChange;
+    receiveChannel.onclose = handleReceiveChannelStatusChange;
+}
+
+function handleSendChannelStatusChange(event) {
+    if (sendChannel) {
+        var state = sendChannel.readyState;
+
+        if (state === "open") {
+            messageInputBox.disabled = false;
+            messageInputBox.focus();
+            sendButton.disabled = false;
+            disconnectButton.disabled = false;
+            connectButton.disabled = true;
+        } else {
+            messageInputBox.disabled = true;
+            sendButton.disabled = true;
+            connectButton.disabled = false;
+            disconnectButton.disabled = true;
+        }
+    }
+}
+
+function handleReceiveChannelStatusChange(event) {
+    if (receiveChannel) {
+        console.log("Receive channel's status has changed to " +
+            receiveChannel.readyState);
+    }
+}
+
+function sendMessage() {
+    var message = messageInputBox.value;
+    sendChannel.send(message);
+
+    messageInputBox.value = "";
+    messageInputBox.focus();
+}
+
+function handleReceiveMessage(event) {
+    var el = document.createElement("p");
+    var txtNode = document.createTextNode(event.data);
+
+    el.appendChild(txtNode);
+    receiveBox.appendChild(el);
+}
+
+function disconnectPeers() {
+
+    // Close the RTCDataChannels if they're open.
+
+    sendChannel.close();
+    receiveChannel.close();
+
+    // Close the RTCPeerConnections
+
+    localConnection.close();
+    remoteConnection.close();
+
+    sendChannel = null;
+    receiveChannel = null;
+    localConnection = null;
+    remoteConnection = null;
+
+    // Update user interface elements
+
+    connectButton.disabled = false;
+    disconnectButton.disabled = true;
+    sendButton.disabled = true;
+
+    messageInputBox.value = "";
+    messageInputBox.disabled = true;
 }
