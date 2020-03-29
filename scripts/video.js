0409c864115306f7ecd8009ab5bad634b29cc994
randor12 <rynicholas@vt.edu>
2020-03-29 14:59:29 -0400

    video footage

--- scripts/video.js
+++ scripts/video.js
@@ -1,155 +1,63 @@
-window.addEventListener('load', () => {
-    // Chat platform
-    // var Handlebars = require('handlebars');
+/**
+ * Initialize Room ID
+ */
+function getRoomID() {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 10; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    $('#room-id').html('Room ID: ' + result);
 
-    const chatTemplate = Handlebars.compile($('#chat-template').html());
-    const chatContentTemplate = Handlebars.compile($('#chat-content-template').html());
-    const chatEl = $('#chat');
-    const formEl = $('#form');
-    const messages = [];
-    let username;
+    getVideo();
 
-    // Local Video
-    const localImageEl = $('#local-image');
-    const localVideoEl = $('#local-video');
+}
 
-    // Remote Videos
-    const remoteVideoTemplate = Handlebars.compile($('#remote-video-template').html());
-    const remoteVideosEl = $('#remote-videos');
-    let remoteVideosCount = 0;
+function getVideo() {
+    // Older browsers might not implement mediaDevices at all, so we set an empty object first
+    if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+    }
 
-    // Hide cameras until they are initialized
-    localVideoEl.hide();
+    // Some browsers partially implement mediaDevices. We can't just assign an object
+    // with getUserMedia as it would overwrite existing properties.
+    // Here, we will just add the getUserMedia property if it's missing.
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function (constraints) {
 
-    // Add validation rules to Create/Join Room Form
-    formEl.form({
-        fields: {
-            roomName: 'empty',
-            username: 'empty',
-        },
-    });
+            // First get ahold of the legacy getUserMedia, if present
+            var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-    // create our webrtc connection
-    const webrtc = new SimpleWebRTC({
-        // the id/element dom element that will hold "our" video
-        localVideoEl: 'local-video',
-        // the id/element dom element that will hold remote videos
-        remoteVideosEl: 'remote-videos',
-        // immediately ask for camera access
-        autoRequestMedia: true,
-        debug: false,
-        detectSpeakingEvents: true,
-        autoAdjustMic: false,
-    });
-
-    // We got access to local camera
-    webrtc.on('localStream', () => {
-        localImageEl.hide();
-        localVideoEl.show();
-    });
-
-    // Remote video was added
-    webrtc.on('videoAdded', (video, peer) => {
-        // eslint-disable-next-line no-console
-        const id = webrtc.getDomId(peer);
-        const html = remoteVideoTemplate({ id });
-        if (remoteVideosCount === 0) {
-            remoteVideosEl.html(html);
-        } else {
-            remoteVideosEl.append(html);
-        }
-        $(`#${id}`).html(video);
-        $(`#${id} video`).addClass('ui image medium'); // Make video element responsive
-        remoteVideosCount += 1;
-    });
-
-    // Update Chat Messages
-    const updateChatMessages = () => {
-        const html = chatContentTemplate({ messages });
-        const chatContentEl = $('#chat-content');
-        chatContentEl.html(html);
-        // automatically scroll downwards
-        const scrollHeight = chatContentEl.prop('scrollHeight');
-        chatContentEl.animate({ scrollTop: scrollHeight }, 'slow');
-    };
-
-    // Post Local Message
-    const postMessage = (message) => {
-        const chatMessage = {
-            username,
-            message,
-            postedOn: new Date().toLocaleString('en-GB'),
-        };
-        // Send to all peers
-        webrtc.sendToAll('chat', chatMessage);
-        // Update messages locally
-        messages.push(chatMessage);
-        $('#post-message').val('');
-        updateChatMessages();
-    };
-
-    // Display Chat Interface
-    const showChatRoom = (room) => {
-        formEl.hide();
-        const html = chatTemplate({ room });
-        chatEl.html(html);
-        const postForm = $('form');
-        postForm.form({
-            message: 'empty',
-        });
-        $('#post-btn').on('click', () => {
-            const message = $('#post-message').val();
-            postMessage(message);
-        });
-        $('#post-message').on('keyup', (event) => {
-            if (event.keyCode === 13) {
-                const message = $('#post-message').val();
-                postMessage(message);
+            // Some browsers just don't implement it - return a rejected promise with an error
+            // to keep a consistent interface
+            if (!getUserMedia) {
+                return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
             }
+
+            // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+            return new Promise(function (resolve, reject) {
+                getUserMedia.call(navigator, constraints, resolve, reject);
+            });
+        }
+    }
+
+    navigator.mediaDevices.getUserMedia({ audio: true, video: {width: 1920, height: 1080, frameRate: 60, facingMode: 'user'} })
+        .then(function (stream) {
+            var video = document.querySelector('video');
+            // Older browsers may not have srcObject
+            if ("srcObject" in video) {
+                console.log("Adding stream")
+                video.srcObject = stream;
+            } else {
+                // Avoid using this in new browsers, as it is going away.
+                video.src = window.URL.createObjectURL(stream);
+            }
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+        })
+        .catch(function (err) {
+            console.log(err.name + ": " + err.message);
         });
-    };
-
-    // Register new Chat Room
-    const createRoom = (roomName) => {
-        // eslint-disable-next-line no-console
-        console.info(`Creating new room: ${roomName}`);
-        webrtc.createRoom(roomName, (err, name) => {
-            formEl.form('clear');
-            showChatRoom(name);
-            postMessage(`${username} created chatroom`);
-        });
-    };
-
-    // Join existing Chat Room
-    const joinRoom = (roomName) => {
-        // eslint-disable-next-line no-console
-        console.log(`Joining Room: ${roomName}`);
-        webrtc.joinRoom(roomName);
-        showChatRoom(roomName);
-        postMessage(`${username} joined chatroom`);
-    };
-
-    // Receive message from remote user
-    webrtc.connection.on('message', (data) => {
-        if (data.type === 'chat') {
-            const message = data.payload;
-            messages.push(message);
-            updateChatMessages();
-        }
-    });
-
-    // Room Submit Button Handler
-    $('.submit').on('click', (event) => {
-        if (!formEl.form('is valid')) {
-            return false;
-        }
-        username = $('#username').val();
-        const roomName = $('#roomName').val().toLowerCase();
-        if (event.target.id === 'create-btn') {
-            createRoom(roomName);
-        } else {
-            joinRoom(roomName);
-        }
-        return false;
-    });
-});
+}
