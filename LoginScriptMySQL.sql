568f5c3db9967373d7b1fe32f5a82ce413043bbf
randor12 <rynicholas@vt.edu>
2020-05-19 10:30:11 -0400

    Update LoginScriptMySQL.sql

--- LoginScriptMySQL.sql
+++ LoginScriptMySQL.sql
@@ -1,20 +1,21 @@
-CREATE DATABASE IF NOT EXISTS schoolshare_schema DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-use schoolshare_schema;
+CREATE DATABASE IF NOT EXISTS sarlaac DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+use sarlaac;
 
 CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` int(11) NOT NULL,
+  `loginid` int(11) NOT NULL PRIMARY KEY,
   `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `salt` varchar(100) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(100) NOT NULL,
+  `GUID` varchar(500) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`, `salt`) VALUES (1, 'test', 'test', 'test@test.com', 'testSalt');
+-- INSERT INTO `accounts` (`id`, `username`, `password`, `email`, `salt`) VALUES (1, 'Ryan', 'example', 'rynicholas@vt.edu', 'testSalt');
 
-ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
-ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+-- ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
+-- ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 
 
-SELECT * FROM accounts where email = "test@gmail.com" and password = "t3stP@ss0rd";
+-- SELECT * FROM accounts where email = "test@gmail.com" and password = "t3stP@ss0rd";
 
-select * from accounts;
+-- select * from accounts;
